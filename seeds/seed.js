6be905a13c02e548676afe1b7de4279f0771883e
Jacob Karla <jacob.l.karla@gmail.com>
2022-09-14 12:09:09 -0400

    created functions to add seeds to db

---
+++ seeds/seed.js
@@ -0,0 +1,12 @@
+const sequelize = require('../config/connection');
+const seedFood = require('./foodData');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedFood();
+
+  process.exit(0);
+};
+
+seedAll();
